4bf642815c08990c95d6fc08861363f25df352aa
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-08-10 15:11:33 -0400

    Add project files.

---
+++ ASP.NETCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NETCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
